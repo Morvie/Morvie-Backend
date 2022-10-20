da5fde2686f1c5f144286d0b3b18d7c320e1db7e
Mark Goertz <58692211+markgoertz@users.noreply.github.com>
2022-10-20 12:46:17 +0200

    Keycloak implementation with React.js

--- APIgateways/OcelotAPIGateway/Program.cs
+++ APIgateways/OcelotAPIGateway/Program.cs
@@ -1,8 +1,31 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using System.Net;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Adding JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+        {
+            //Define which claim requires to check
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            //Stored the value in appsetting.json.
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]))
+        };
+    });
+
 builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
     .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
     .AddEnvironmentVariables();
@@ -22,6 +45,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 await app.UseOcelot();
 app.Run();
