ab03ce378efff0c010061dd0f4ffa2df8dde4818
Mark Goertz <58692211+markgoertz@users.noreply.github.com>
2022-10-27 19:08:04 +0200

    Update Program.cs

--- backend/Microservices/Review/ReviewService/Program.cs
+++ backend/Microservices/Review/ReviewService/Program.cs
@@ -1,5 +1,6 @@
 using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 using MorvieReview.Application.Interfaces;
 using MorvieReview.Infrastructure.Repositories;
@@ -10,7 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options => options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
