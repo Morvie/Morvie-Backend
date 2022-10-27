901dd85bb4954f406b0c2b6531d3cd106209dc29
Mark Goertz <58692211+markgoertz@users.noreply.github.com>
2022-10-27 11:11:05 +0200

    Added Keycloak implementation
    Keycloak has been added to this project and works for front-end authentication and backend authentication and authorization.

--- APIgateways/OcelotAPIGateway/Program.cs
+++ APIgateways/OcelotAPIGateway/Program.cs
@@ -1,43 +1,62 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System.Net;
-using System.Text;
+using OcelotAPIGateway;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//Adding JWT Authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            //Define which claim requires to check
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
 
-            //Stored the value in appsetting.json.
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Issuer"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:key"]))
-        };
-    });
+const string corsPolicyName = "cors-app-policy";
 
-builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
-    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
-builder.Services.AddOcelot(builder.Configuration);
-// Add services to the container.
+builder.Configuration.AddJsonFile("Ocelot.json");
+
+//Add services to the container.
+//Add Ocelot configurations.
+
+builder.Services.AddCors(c => c.AddPolicy("CORS_policy", corsPolicyBuilder =>
+{
+    corsPolicyBuilder.WithOrigins("http://localhost:3000")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
+
+//Configure JWT
+builder.Services.ConfigureJWT(builder.Environment.IsDevelopment(), "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAidhPtqqZxoWtonBScBTh5PU5aj5gB9rMzpqeuohOnBLxIS8k+bxR4hM4SG2cFAiF/A/e7fJmhk7Xz8OtK8W0tUFc4ilnqOJvVtI8J39++ZU3PmTu3p+hyjjB96rF0O2Y3suOz8fxQSgUTMbTp2V0pJyStmC+8MlSptIQ7NvZA3dJgUiqF5L/u06huSEstpGhld1QZU4zu7QUKCBs/qn6orjxido3+rfaLIlUlnohFrdqRGBXDVQcfrR+09FirdMc/0xCKmkuwUZUMRZLqXk4ZkhxO1GGieOifCTPt1bV0KUDYsxXZny+rb8+Fl8FO1UFNcmuCtEZNLblD7mJJK9Z+wIDAQAB");
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Morvie-Keycloak", Version = "v1" });
+
+    //First we define the security scheme
+    c.AddSecurityDefinition("Bearer", //Name the security scheme
+    new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Type = SecuritySchemeType.Http, //We set the scheme type to http since we're using bearer authentication
+        Scheme = JwtBearerDefaults.AuthenticationScheme //The name of the HTTP Authorization scheme to be used in the Authorization header. In this case "bearer".
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+            {
+                new OpenApiSecurityScheme{
+                    Reference = new OpenApiReference{
+                    Id = JwtBearerDefaults.AuthenticationScheme, //The name of the previously defined security scheme.
+                    Type = ReferenceType.SecurityScheme
+                }
+            },new List<string>()
+        }
+    });
+});
+
+builder.Services.AddOcelot(builder.Configuration);
+
 
 var app = builder.Build();
+
+app.UseCors("CORS_policy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -45,8 +64,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
-await app.UseOcelot();
+app.UseEndpoints(endpoints =>
+{
+    app.MapControllers();
+});
+app.UseOcelot().Wait();
 app.Run();
--- backend/Microservices/Review/ReviewService/Program.cs
+++ backend/Microservices/Review/ReviewService/Program.cs
@@ -1,4 +1,6 @@
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
 using MorvieReview.Application.Interfaces;
 using MorvieReview.Infrastructure.Repositories;
 using CommandsMediatR = MorvieReview.Application.Commands;
@@ -11,7 +13,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Morvie-Keycloak", Version = "v1" });
+
+    //First we define the security scheme
+    c.AddSecurityDefinition("Bearer", //Name the security scheme
+    new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Type = SecuritySchemeType.Http, //We set the scheme type to http since we're using bearer authentication
+        Scheme = JwtBearerDefaults.AuthenticationScheme //The name of the HTTP Authorization scheme to be used in the Authorization header. In this case "bearer".
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+            {
+                new OpenApiSecurityScheme{
+                    Reference = new OpenApiReference{
+                    Id = JwtBearerDefaults.AuthenticationScheme, //The name of the previously defined security scheme.
+                    Type = ReferenceType.SecurityScheme
+                }
+            },new List<string>()
+        }
+    });
+});
 
 // Dependency injections via MediatR:
 builder.Services.AddMediatR(new Type[]
@@ -36,6 +61,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
